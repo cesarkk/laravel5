345b6a29221ac143f7e6e7a73bb5d2a6905ba72e
cesarkk <kodacesar38@gmail.com>
2021-11-11 21:16:18 -0500

    laravelver0

---
+++ database/migrations/2021_11_12_004913_create_empleados_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmpleadosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('Nombre',50);  
+            $table->string('ApellidoPaterno',40); 
+            $table->string('ApellidoMaterno',40); 
+            $table->string('Correo',100); 
+            $table->string('Foto');     
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+}
